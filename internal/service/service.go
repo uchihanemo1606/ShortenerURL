97a9555625ab29f78dc039f1fead9ecca285a057
Nemo <76892046+uchihanemo1606@users.noreply.github.com>
2025-12-29 08:24:03 +0700

    feat(service): add existing short code check and improve error logging

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,14 +2,16 @@ package service
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
+	"fmt"
+	// "hash"
 	"log"
 	"time"
 	"urlshortener/internal/models"
 	"urlshortener/internal/storage"
-
 	"github.com/redis/go-redis/v9"
 )
 
@@ -25,6 +27,11 @@ func NewShortenerService(store *storage.RedisStore) *ShortenerService {
 
 // ShortenURL tạo short code cho URL dài
 func (s *ShortenerService) ShortenURL(longURL string) (string, error) {
+	
+	if shortCode, exits := s.GetExitingShortCode(longURL); exits {
+		return shortCode, nil
+	}
+
 	// Tạo short code ngẫu nhiên 6 ký tự
 	shortCode := generateShortCode()
 
@@ -67,7 +74,7 @@ func (s *ShortenerService) GetLongURL(shortCode string) (string, bool) {
 
 	updateData, err := json.Marshal(url)
 	if err != nil {
-		log.Printf("❌ Lỗi khi cập nhật lượt click: %v", err)
+		log.Printf("Error when increasing clicks : %v", err)
 		return "", false
 	}
 	err = s.store.GetClient().Set(s.store.GetContext(), key, updateData, 0).Err()
@@ -82,4 +89,20 @@ func generateShortCode() string {
 	bytes := make([]byte, 6)
 	rand.Read(bytes)
 	return base64.URLEncoding.EncodeToString(bytes)[:6]
+}
+
+
+func (s *ShortenerService) GetExitingShortCode(longURL string) (string, bool) {
+	hash := sha256.Sum256([]byte(longURL))
+	key := s.store.GetPrefix() + "long:" + fmt.Sprintf("%x", hash)
+
+	shortCode, err := s.store.GetClient().Get(s.store.GetContext(), key).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil){
+			return "", false
+		}
+		log.Printf("Error: check exit long url: %v", err)
+		return "", false
+	}
+	return shortCode, true
 }
